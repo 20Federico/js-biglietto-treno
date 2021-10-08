9a6fd8e5eadf7f50cc7a14d4953c9e0bd70bf809
20Federico <fefe.molino@gmail.com>
2021-10-08 17:10:07 +0200

    moved .toFixed(2) to the const

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ alert('Benvenuto, per calcolare il prezzo del tuo biglietto, clicca ok');
 
 const km = prompt('Quanti chilometri devi percorrere?');
 const age = prompt('Quanti anni hai?');
-const price = 0.21 * km;
-const priceYoung = price - (price / 5);
-const priceOld = price - (price * 2 / 5);
+const price = (0.21 * km).toFixed(2);
+const priceYoung = (price - (price / 5)).toFixed(2);
+const priceOld = (price - (price * 2 / 5)).toFixed(2);
 const msgPrice = 'Il prezzo del tuo biglietto è di ';
 
 if (isNaN(km) || isNaN(age) || km < 0 || age < 0) {
@@ -13,11 +13,11 @@ if (isNaN(km) || isNaN(age) || km < 0 || age < 0) {
 } else {
 
   if (age <= 18) {
-    alert(msgPrice + priceYoung.toFixed(2) + '€.');
+    alert(msgPrice + priceYoung + '€.');
   } else if (age >= 65) {
-    alert(msgPrice + priceOld.toFixed(2) + '€.');
+    alert(msgPrice + priceOld + '€.');
   } else {
-    alert(msgPrice + price.toFixed(2) + '€.');
+    alert(msgPrice + price + '€.');
   }
 
 }
